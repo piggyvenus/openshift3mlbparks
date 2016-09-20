caa7b0a66b72d314f5ab59938737471abff2dda9
piggyvenus <piggyvenus@gmail.com>
2016-09-20 18:42:40 -0400

    Add files via upload

---
+++ src/main/java/org/openshift/mlbparks/domain/Scotia.java
@@ -0,0 +1,149 @@
+package org.openshift.mlbparks.domain;
+
+public class Scotia {
+	private Object storenum;
+	private Object storename;
+	private Object storetype;
+	private Object address;
+	private Object city;
+	private Object state;
+	private Object zip;
+	private Object phone;
+	private Object fax;
+	private Object storehours;
+	private Object latitude;
+	private Object longitude;
+	private Object geo;
+	private Object country;
+	private Object countrycode;
+	private Object county;
+
+	public Object getStorenum() {
+		return storenum;
+	}
+
+	public void setStorenum(Object storenum) {
+		this.storenum = storenum;
+	}
+
+	public Object getstorename() {
+		return storename;
+	}
+
+	public void setstorename(Object storename) {
+		this.storename = storename;
+	}
+
+	public Object getStoretype() {
+		return storetype;
+	}
+
+	public void setStoretype(Object storetype) {
+		this.storetype = storetype;
+	}
+
+	public Object getAddress() {
+		return address;
+	}
+
+	public void setAddress(Object address) {
+		this.address = address;
+	}
+
+	public Object getCity) {
+		return city;
+	}
+
+	public void setCity(Object city) {
+		this.city = city;
+	}
+
+	public Object getState() {
+		return state;
+	}
+
+	public void setState(Object state) {
+		this.state = state;
+	}
+
+	public Object getZip() {
+		return zip;
+	}
+
+	public void setZip(Object zip) {
+		this.zip = zip;
+	}
+
+	public Object getPhone() {
+		return phone;
+	}
+
+	public void setPhone(Object phone) {
+		this.phone = phone;
+	}
+
+	public Object getFax() {
+		return fax;
+	}
+
+	public void setFax(Object fax) {
+		this.fax = fax;
+	}
+
+	public Object getStorehours() {
+		return storehours;
+	}
+
+	public void setStorehours(Object storehours) {
+		this.storehours = storehours;
+	}
+
+	public Object getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(Object latitude) {
+		this.latitude = latitude;
+	}
+
+	public Object getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(Object longitude) {
+		this.longitude = longitude;
+	}
+
+	public Object getGeo() {
+		return geo;
+	}
+
+	public void setGeo(Object geo) {
+		this.geo = geo;
+	}
+
+	public Object getCountry() {
+		return country;
+	}
+
+	public void setCountry(Object country) {
+		this.country = country;
+	}
+
+	public Object getCountrycode() {
+		return countrycode;
+	}
+
+	public void setCountrycode(Object countrycode) {
+		this.countrycode = countrycode;
+	}
+
+	public Object getCounty() {
+		return county;
+	}
+
+	public void setCounty(Object county) {
+		this.county = county;
+	}
+
+}
